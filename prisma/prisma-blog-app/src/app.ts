3b4698998d0b0932288f4570688e796db1cb1fdf
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-12-29 09:14:20 +0600

    Post create api created

--- prisma/prisma-blog-app/src/app.ts
+++ prisma/prisma-blog-app/src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from "express";
+import { postRouter } from "./modules/post/post.router";
 
 const app: Application = express();
+
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.json({
