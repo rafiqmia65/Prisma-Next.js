dd3e88b24ec2e799a95e39f147967ff7d913d6a9
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-12-30 15:08:56 +0600

    Better Auth Setup

--- prisma/prisma-blog-app/src/app.ts
+++ prisma/prisma-blog-app/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use("/posts", postRouter);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.json({
