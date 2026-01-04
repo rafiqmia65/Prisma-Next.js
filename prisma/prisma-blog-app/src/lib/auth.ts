063140e6874b5e4b90b2f1fff5491dc1fc5a09d5
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-01-04 18:52:04 +0600

    feat: add authentication middleware with role-based access

--- prisma/prisma-blog-app/src/lib/auth.ts
+++ prisma/prisma-blog-app/src/lib/auth.ts
@@ -44,6 +44,7 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendOnSignUp: true,
+    autoSignInAfterVerification: true,
     sendVerificationEmail: async ({ user, url, token }, request) => {
       try {
         const VerificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
@@ -148,6 +149,8 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
+      prompt: "select_account consent",
+      accessType: "offline",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
