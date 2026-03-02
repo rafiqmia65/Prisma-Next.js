ef6c70ae20252d627ace395e1b2d5cef0db2e415
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-03-02 16:08:16 +0600

    feat: Get me session data route added

--- ph-healthcare/ph-healthcare-server/src/app/modules/auth/auth.route.ts
+++ ph-healthcare/ph-healthcare-server/src/app/modules/auth/auth.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../../generated/prisma/enums";
 
 const AuthRoutes: Router = Router();
 
 AuthRoutes.post("/register", AuthController.registerPatient);
 AuthRoutes.post("/login", AuthController.loginUser);
+AuthRoutes.get(
+  "/me",
+  checkAuth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
+  AuthController.getMe,
+);
 
 export default AuthRoutes;
--- ph-healthcare/ph-healthcare-server/src/app/routes/indexRoutes.ts
+++ ph-healthcare/ph-healthcare-server/src/app/routes/indexRoutes.ts
@@ -3,6 +3,7 @@ import SpecialtyRoutes from "../modules/specialty/specialty.route";
 import AuthRoutes from "../modules/auth/auth.route";
 import doctorRoutes from "../modules/doctor/doctor.route";
 import userRoutes from "../modules/user/user.route";
+import AdminRoutes from "../modules/admin/admin.route";
 
 //  type the router
 const indexRoutes: Router = Router();
@@ -12,6 +13,9 @@ indexRoutes.use("/auth", AuthRoutes);
 
 // User Routes
 indexRoutes.use("/users", userRoutes);
+
+// Admin Routes
+indexRoutes.use("admin", AdminRoutes);
 
 // Specialty Route
 indexRoutes.use("/specialties", SpecialtyRoutes);
