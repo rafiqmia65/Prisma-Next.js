31fdad39ee7a86ad505c0b59c74c70c66973ba94
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-03-16 17:42:01 +0600

    feat: setup routing, layouts, shadcn UI, axios client and tanstack query with SSR prefetch

---
+++ ph-healthcare/ph-healthcare-frontend/src/app/(commonLayout)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+        Common file for all pages in the common layout
+      <Button variant="outline">Hello World</Button>
+    </div>
+  );
+}
